7fb12bbd15bebfde5eac13bc4f597bd07059bec9
sofia <sofia.jimlap@educa.jcyl.es>
2023-12-05 20:36:15 +0100

    segundo proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CabeceraComponent} from "./componentes/cabecera/cabecera.component";
+import {CuerpoComponent} from "./componentes/cuerpo/cuerpo.component";
+import {PieComponent} from "./componentes/pie/pie.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CabeceraComponent, CuerpoComponent, PieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  misCabeceras: string[] = ["Titulo", "Autor"];
-  misDatosL: any[] = [
-    { "Titulo": "El quijote", "Autor": "Cervantes" },
-    { "Titulo": "100 años de soledad", "Autor": "Gabriel García Márquez" },
-    { "Titulo": "Los pilares de la Tierra", "Autor": "Ken Follet" }
-  ];
-  misDatosR: any[] = [
-    { "Titulo": "Jara y sedal", "Autor": "A.Lopez" },
-    { "Titulo": "Pc Computer", "Autor": "MA Editorial" },
-    { "Titulo": "Muy Interesante", "Autor": "SA Edition" }
-  ];
-  misDatosM: any[] = [
-    { "Titulo": "Rapsody", "Autor": "Queen" },
-    { "Titulo": "Motomamy", "Autor": "Rosalía" },
-    { "Titulo": "Despechada", "Autor": "Shakira" }
-  ];
-  misDatosC: any[] = [
-    { "Titulo": "Napoleón", "Autor": "Ridley Scoot" },
-    { "Titulo": "Frozen", "Autor": "Jennifer Lee" },
-    { "Titulo": "Titanic", "Autor": "James Cameron" }
-  ];
+
 }
--- src/app/componentes/cuerpo/cuerpo.component.ts
+++ src/app/componentes/cuerpo/cuerpo.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {TablaComponent} from "../tabla/tabla.component";
 
 @Component({
   selector: 'app-cuerpo',
   standalone: true,
-  imports: [],
+  imports: [
+    TablaComponent
+  ],
   templateUrl: './cuerpo.component.html',
   styleUrl: './cuerpo.component.css'
 })
 export class CuerpoComponent {
-
+  misCabeceras: string[] = ["Titulo", "Autor"];
+  misDatosL: any[] = [
+    { "Titulo": "El quijote", "Autor": "Cervantes" },
+    { "Titulo": "100 años de soledad", "Autor": "Gabriel García Márquez" },
+    { "Titulo": "Los pilares de la Tierra", "Autor": "Ken Follet" }
+  ];
+  misDatosR: any[] = [
+    { "Titulo": "Jara y sedal", "Autor": "A.Lopez" },
+    { "Titulo": "Pc Computer", "Autor": "MA Editorial" },
+    { "Titulo": "Muy Interesante", "Autor": "SA Edition" }
+  ];
+  misDatosM: any[] = [
+    { "Titulo": "Rapsody", "Autor": "Queen" },
+    { "Titulo": "Motomamy", "Autor": "Rosalía" },
+    { "Titulo": "Despechada", "Autor": "Shakira" }
+  ];
+  misDatosC: any[] = [
+    { "Titulo": "Napoleón", "Autor": "Ridley Scoot" },
+    { "Titulo": "Frozen", "Autor": "Jennifer Lee" },
+    { "Titulo": "Titanic", "Autor": "James Cameron" }
+  ];
 }
